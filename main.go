e63d44c92ace8d7b5af0c24e39b05dee5f836e09
mahcks <maxnatural13@gmail.com>
2022-12-07 20:10:47 -0600

    updated workflow

--- main.go
+++ main.go
@@ -4,8 +4,16 @@ Copyright © 2022 NAME HERE <EMAIL ADDRESS>
 */
 package main
 
-import "github.com/Mahcks/nba-lottery/cmd"
+import (
+	"fmt"
+
+	"github.com/Mahcks/nba-lottery/cmd"
+)
+
+var Version string
 
 func main() {
+	fmt.Println(Version)
+
 	cmd.Execute()
 }
